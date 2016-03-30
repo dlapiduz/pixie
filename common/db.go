f2b3758dff2455c93a7a517541a8ebd14b2b8bc3
Diego Lapiduz <diego@lapiduz.com>
2016-03-29 20:15:38 -0500

    Sqlite for testing

--- common/db.go
+++ common/db.go
@@ -3,13 +3,27 @@ package common
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func LoadDB() (*gorm.DB, error) {
-	db, err := gorm.Open("postgres", os.Getenv("PSQL_CONN"))
+	var err error
+	var db gorm.DB
+
+	if strings.Contains(os.Args[0], "test") {
+		// We are doing testing!
+		log.Println("Connecting to TEST DB")
+		db, err = gorm.Open("sqlite3", ":memory:")
+	} else {
+		log.Println("Connecting to DB")
+		db, err = gorm.Open("postgres", os.Getenv("PSQL_CONN"))
+		// DB.LogMode(true)
+		db.DB().SetMaxOpenConns(10)
+	}
 
 	if err != nil {
 		log.Println("Error connecting to DB")
